d10ca1d674fccb107e93609a0e62d57cd3e2497f
Rodanger <rodangermr@gmail.com>
2022-02-14 17:12:20 -0500

    first commit

--- ecommerce/ecommerce/store/views.py
+++ ecommerce/ecommerce/store/views.py
@@ -43,14 +43,15 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
         
     else:
         #Create Empty car for now for none-logged in users
         items = []
         order - {'get_cart_total':0, 'get_cart_items':0}
-       
+        cartItems = order['get_cart_items']
     
-    context = {'items':items, 'order':order}
+    context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'store/checkout.html', context)
 
 
